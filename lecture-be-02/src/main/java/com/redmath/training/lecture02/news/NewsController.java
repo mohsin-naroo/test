eab5341c26db1be29215afa61a1bda824cf3c4b5
Mohsin Ali <mohsin-naroo@users.noreply.github.com>
2024-07-04 09:52:14 +0500

    add lecture-be-02

---
+++ lecture-be-02/src/main/java/com/redmath/training/lecture02/news/NewsController.java
@@ -0,0 +1,35 @@
+package com.redmath.training.lecture02.news;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewsController {
+
+    private final NewsRepository newsRepository;
+
+    public NewsController(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    @GetMapping("/api/v1/news/{newsId}")
+    public ResponseEntity<News> get(@PathVariable("newsId") Long newsId) {
+        Optional<News> news = newsRepository.findById(newsId);
+        if (news.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(news.get());
+    }
+
+    @GetMapping("/api/v1/news")
+    public ResponseEntity<List<News>> get(@RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "page", defaultValue = "1000") Integer size) {
+        return ResponseEntity.ok(newsRepository.findAll());
+    }
+}
